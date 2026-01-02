3e4e059a60ea21e9ff8100ad1a97f0a105039ae5
orsi <jonathon.orsi@gmail.com>
2026-01-02 14:28:46 -0500

    chore: add back in jsruntime test

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -442,67 +442,255 @@ function test(
 //   }
 // );
 
-let RemoveRowReducer: [
-  {
-    id: number;
-    label: string;
-  }[],
-  any
-];
+// let RemoveRowReducer: [
+//   {
+//     id: number;
+//     label: string;
+//   }[],
+//   any
+// ];
+// test(
+//   "RemoveRowReducerTest",
+//   () => {
+//     const RemoveRowReducerTest = () => {
+//       const [data] = (RemoveRowReducer = useReducer(
+//         (state, action) => {
+//           switch (action.type) {
+//             case "REMOVE": {
+//               const idx = state.findIndex((d: any) => d.id === action.id);
+//               return [...state.slice(0, idx), ...state.slice(idx + 1)];
+//             }
+//             default:
+//               return state;
+//           }
+//         },
+//         [
+//           {
+//             id: 1,
+//             label: `item-1`,
+//           },
+//           {
+//             id: 2,
+//             label: `item-2`,
+//           },
+//           {
+//             id: 3,
+//             label: `item-3`,
+//           },
+//         ]
+//       ));
+
+//       return (
+//         <div id="container">
+//           <div id="before">Before</div>
+//           {data.map((item) => (
+//             <div id={`item-${item.id}`}>
+//               {item.id}
+//               {/* Why does adding this nested <td> break? */}
+//               {/* <td class="col-md-1">{item.id}</td> */}
+//             </div>
+//           ))}
+//           <div id="after">After</div>
+//         </div>
+//       );
+//     };
+//     return <RemoveRowReducerTest />;
+//   },
+//   ($container) => {
+//     RemoveRowReducer[1]({ type: "REMOVE", id: 2 });
+//     return (
+//       $container.querySelector("#before") != null &&
+//       $container.querySelector("#item-1") != null &&
+//       $container.querySelector("#item-2") == null &&
+//       $container.querySelector("#item-3") != null &&
+//       $container.querySelector("#after") != null
+//     );
+//   }
+// );
+
+let JSRuntimeReducer: any;
 test(
-  "RemoveRowReducerTest",
+  "JSRuntimeTest",
   () => {
-    const RemoveRowReducerTest = () => {
-      const [data] = (RemoveRowReducer = useReducer(
-        (state, action) => {
-          switch (action.type) {
-            case "REMOVE": {
-              const idx = state.findIndex((d: any) => d.id === action.id);
-              return [...state.slice(0, idx), ...state.slice(idx + 1)];
-            }
-            default:
-              return state;
+    const random = (max: number) => Math.round(Math.random() * 1000) % max;
+    const A = [
+      "pretty",
+      "large",
+      "big",
+      "small",
+      "tall",
+      "short",
+      "long",
+      "handsome",
+      "plain",
+      "quaint",
+      "clean",
+      "elegant",
+      "easy",
+      "angry",
+      "crazy",
+      "helpful",
+      "mushy",
+      "odd",
+      "unsightly",
+      "adorable",
+      "important",
+      "inexpensive",
+      "cheap",
+      "expensive",
+      "fancy",
+    ];
+    const C = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
+    const N = [
+      "table",
+      "chair",
+      "house",
+      "bbq",
+      "desk",
+      "car",
+      "pony",
+      "cookie",
+      "sandwich",
+      "burger",
+      "pizza",
+      "mouse",
+      "keyboard",
+    ];
+
+    let nextId = 1;
+
+    const buildData = (
+      count: number
+    ): {
+      id: number;
+      label: string;
+    }[] => {
+      const data = new Array(count);
+
+      for (let i = 0; i < count; i++) {
+        data[i] = {
+          id: nextId++,
+          label: `${A[random(A.length)]} ${C[random(C.length)]} ${N[random(N.length)]}`,
+        };
+      }
+
+      return data;
+    };
+
+    const initialState = { data: [], selected: 0 } as {
+      data: ReturnType<typeof buildData>;
+      selected: number;
+    };
+
+    const listReducer = (state: typeof initialState, action: any) => {
+      const { data, selected } = state;
+
+      switch (action.type) {
+        case "RUN":
+          return { data: buildData(1000), selected: 0 };
+        case "RUN_LOTS":
+          return { data: buildData(10000), selected: 0 };
+        case "ADD":
+          return { data: data.concat(buildData(1000)), selected };
+        case "UPDATE": {
+          const newData = data.slice(0);
+
+          for (let i = 0; i < newData.length; i += 10) {
+            const r = newData[i];
+
+            newData[i] = { id: r.id, label: r.label + " !!!" };
           }
-        },
-        [
-          {
-            id: 1,
-            label: `item-1`,
-          },
-          {
-            id: 2,
-            label: `item-2`,
-          },
-          {
-            id: 3,
-            label: `item-3`,
-          },
-        ]
-      ));
+
+          return { data: newData, selected };
+        }
+        case "CLEAR":
+          return { data: [], selected: 0 };
+        case "SWAP_ROWS":
+          const newdata = [...data];
+          if (data.length > 998) {
+            const d1 = newdata[1];
+            const d998 = newdata[998];
+            newdata[1] = d998;
+            newdata[998] = d1;
+          }
+          return { data: newdata, selected };
+        case "REMOVE": {
+          const idx = data.findIndex((d: any) => d.id === action.id);
+
+          return {
+            data: [...data.slice(0, idx), ...data.slice(idx + 1)],
+            selected,
+          };
+        }
+        case "SELECT":
+          return { data, selected: action.id };
+        default:
+          return state;
+      }
+    };
+
+    const Row = ({ selected, item, dispatch }: any) => (
+      <tr class={`row${selected ? " danger" : ""}`}>
+        <td class="col-md-1">{item.id}</td>
+        <td class="col-md-4">
+          <a onClick={() => dispatch({ type: "SELECT", id: item.id })}>{item.label}</a>
+        </td>
+        <td class="col-md-1">
+          <a onClick={() => dispatch({ type: "REMOVE", id: item.id })}>
+            <span class="glyphicon glyphicon-remove" aria-hidden="true" />
+          </a>
+        </td>
+        <td class="col-md-6" />
+      </tr>
+    );
+
+    const Button = ({ id, cb, title }: any) => (
+      <button type="button" class="btn btn-primary btn-block" id={id} onClick={cb}>
+        {title}
+      </button>
+    );
+
+    const Main = () => {
+      JSRuntimeReducer = useReducer(listReducer, initialState);
+      const [{ data, selected }, dispatch] = JSRuntimeReducer;
 
       return (
-        <div id="container">
-          <div id="before">Before</div>
-          {data.map((item) => (
-            <div id={`item-${item.id}`}>
-              <td class="col-md-1">{item.id}</td>
+        <>
+          <div class="jumbotron">
+            <div id="header" class="row">
+              <div class="col-md-6">
+                <h1>JINX</h1>
+              </div>
+              <div class="col-md-6">
+                <div class="row">
+                  <Button id="run" title="Create 1,000 rows" cb={() => dispatch({ type: "RUN" })} />
+                  <Button id="runlots" title="Create 10,000 rows" cb={() => dispatch({ type: "RUN_LOTS" })} />
+                  <Button id="add" title="Append 1,000 rows" cb={() => dispatch({ type: "ADD" })} />
+                  <Button id="update" title="Update every 10th row" cb={() => dispatch({ type: "UPDATE" })} />
+                  <Button id="clear" title="Clear" cb={() => dispatch({ type: "CLEAR" })} />
+                  <Button id="swaprows" title="Swap Rows" cb={() => dispatch({ type: "SWAP_ROWS" })} />
+                </div>
+              </div>
             </div>
-          ))}
-          <div id="after">After</div>
-        </div>
+          </div>
+
+          <table class="table table-hover table-striped test-data">
+            <tbody>
+              {data.map((item: any) => (
+                <Row item={item} selected={selected === item.id} dispatch={dispatch} />
+              ))}
+            </tbody>
+          </table>
+          <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true" />
+        </>
       );
     };
-    return <RemoveRowReducerTest />;
+    return <Main />;
   },
   ($container) => {
-    RemoveRowReducer[1]({ type: "REMOVE", id: 2 });
-    return (
-      $container.querySelector("#before") != null &&
-      $container.querySelector("#item-1") != null &&
-      $container.querySelector("#item-2") == null &&
-      $container.querySelector("#item-3") != null &&
-      $container.querySelector("#after") != null
-    );
+    JSRuntimeReducer[1]({ type: "RUN" });
+    JSRuntimeReducer[1]({ type: "CLEAR" });
+    return $container.querySelector("#header") != null;
   }
 );
 
